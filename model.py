9242596f753e45b0adb629543de13c8e09c91b8c
Nitsan Amit <Nitsan135@gmail.com>
2020-06-10 22:13:50 +0300

    add data partition

--- model.py
+++ model.py
@@ -14,7 +14,14 @@ class FlightPredictor:
         Initialize an object from this class.
         @param path_to_weather: The path to a csv file containing weather data.
         """
-        raise NotImplementedError
+        random.seed(5)
+        raw_data = pd.read_csv('data/train_data.csv')
+        X = raw_data.drop(["ArrDelay", "DelayFactor"], axis=1)
+        y = raw_data[["ArrDelay", "DelayFactor"]]
+        x_temp, x_test, y_temp, y_test = train_test_split(X, y, test_size=0.2)
+        x_train, x_validate, y_train, y_validate = train_test_split(x_temp, y_temp, test_size=0.2)
+
+        # Only use x_train and y_train!!!!!!!!!!!!!!!
 
     def predict(self, x):
         """
