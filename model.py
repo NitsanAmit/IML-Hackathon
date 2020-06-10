0e4f8fafae5c5bd6653fc44630994da5f533a681
noaschaffer <noaschaffer15@gmail.com>
2020-06-10 22:19:25 +0300

    add random

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ Author(s): Nitsan Shahar Gal Noa
 
 ===================================================
 """
-
+import random
 import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
