4566894ae59807ef469b1899c8721dd59e14db65
Nitsan Amit <Nitsan135@gmail.com>
2020-06-10 22:16:54 +0300

    add libs

--- model.py
+++ model.py
@@ -8,6 +8,11 @@ Author(s): Nitsan Shahar Gal Noa
 ===================================================
 """
 
+import pandas as pd
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+
 class FlightPredictor:
     def __init__(self, path_to_weather=''):
         """
