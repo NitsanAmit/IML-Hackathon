fd2225e267568cd481e783553eddbc82c01f8dcf
noaschaffer <noaschaffer15@gmail.com>
2020-06-11 18:58:20 +0300

    clean train

--- model.py
+++ model.py
@@ -33,14 +33,14 @@ class FlightPredictor:
         x_train, self.x_test, y_train, self.y_test = train_test_split(X, y, test_size=0.2)
 
         # Only use x_train and y_train!!!!!!!!!!!!!!!
-        train = self.clean_up_data(path_to_weather, x_train, y_train)
+        train = self.clean_up_train(path_to_weather, x_train, y_train)
+        self._cols_name = train.columns.values.tolist()
         self.y_train_regression = train.loc[:, "ArrDelay"].to_frame()
         self.y_train_classification = train.loc[:, "DelayFactor"].to_frame()
         self.x_train = train.drop(["ArrDelay", "DelayFactor"], axis=1)
 
         # regression
         self._lasso_regression = LassoCV(cv=5, random_state=0).fit(self.x_train, self.y_train_regression.values.ravel())
-        print("1")
 
     # def fit(self, X_train, y_train):
     #     self._lasso_regression.fit(X_train, y_train)
@@ -52,21 +52,37 @@ class FlightPredictor:
         @param x: A pandas DataFrame with shape (m, 15)
         @return: A pandas DataFrame with shape (m, 2) with your prediction
         """
+        df = self.clean_up_test()
         raise NotImplementedError
 
-    def clean_up_data(self, path_to_weather, X, y):
-        joint_df = X.join(y)
+    def clean_up_data(self, joint_df, path_to_weather=None):
         joint_df = remove_outliers(joint_df)
         joint_df = add_arrival_departure_bins(joint_df)
         joint_df = make_flight_date_canonical(joint_df)
         joint_df = factorize_delay(joint_df)
         joint_df = add_is_same_state(joint_df)
-        if path_to_weather:
-            add_weather_data(joint_df, path_to_weather)
+        # if path_to_weather:
+        #     add_weather_data(joint_df, path_to_weather)
         joint_df = get_dummies(joint_df)
-        joint_df = cross_holidays(joint_df)
+        # joint_df = cross_holidays(joint_df)
         joint_df = drop_features(joint_df)
         return joint_df
+
+    def clean_up_train(self, path_to_weather, X, y):
+        joint_df = X.join(y)
+        joint_df = self.clean_up_data(joint_df)
+        return joint_df
+
+
+
+    def clean_up_test(self, X):
+        joint_df = self.clean_up_data(X)
+        
+
+
+def add_bias(df):
+    df.insert(loc=0, column='bias', value=np.ones(df.shape[0]))
+    return df
 
 
 def visualize(df):
@@ -74,23 +90,25 @@ def visualize(df):
     df["is_delayed"] = (df['DelayFactor'] != -1).astype(int)
 
     p1 = (ggplot(df, aes(x='Distance', y='ArrDelay', color='is_delayed')) + geom_point() +
-         labs(title="Distance vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['Distance']))))
+          labs(title="Distance vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['Distance']))))
     print(p1)
 
     p2 = (ggplot(df, aes(x='CRSElapsedTime', y='ArrDelay', color='is_delayed')) + geom_point() +
-         labs(title="CRSElapsedTime vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['CRSElapsedTime']))))
+          labs(
+              title="CRSElapsedTime vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['CRSElapsedTime']))))
     print(p2)
 
     p3 = (ggplot(df, aes(x='CanonicalFlightDate', y='ArrDelay', color='is_delayed')) + geom_point() +
-         labs(title="CanonicalFlightDate vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['CanonicalFlightDate']))))
+          labs(title="CanonicalFlightDate vs. delay time, correlation: {}".format(
+              df['ArrDelay'].corr(df['CanonicalFlightDate']))))
     print(p3)
 
     p4 = (ggplot(df, aes(x='DepBin', y='ArrDelay', color='is_delayed')) + geom_point() +
-         labs(title="DepBin vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['DepBin']))))
+          labs(title="DepBin vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['DepBin']))))
     print(p4)
 
     p5 = (ggplot(df, aes(x='ArrBin', y='ArrDelay', color='is_delayed')) + geom_point() +
-         labs(title="ArrBin vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['ArrBin']))))
+          labs(title="ArrBin vs. delay time, correlation: {}".format(df['ArrDelay'].corr(df['ArrBin']))))
     print(p5)
 
     numeric_columns = pd.concat([df['ArrBin'], df['DepBin'], df['CanonicalFlightDate'], df['CRSElapsedTime'],
@@ -99,11 +117,11 @@ def visualize(df):
     cormat.index.name = 'variable2'
     cormat.reset_index(inplace=True)
     melted_cormat = pd.melt(cormat, id_vars=['variable2'])
-    p_corr = ggplot(melted_cormat, aes(x='variable', y='variable2', fill='value')) + geom_tile()\
+    p_corr = ggplot(melted_cormat, aes(x='variable', y='variable2', fill='value')) + geom_tile() \
              + labs(title="Numeric Columns Correlation Table")
     print(p_corr)
 
-        # df_dest_delayed = df.groupby('Dest').agg({'is_delayed': ['mean']})
+    # df_dest_delayed = df.groupby('Dest').agg({'is_delayed': ['mean']})
 
 
 def remove_outliers(joint_df):
@@ -132,7 +150,7 @@ def get_dummies(joint_df):
 
 def drop_features(joint_df):
     return joint_df.drop(['Tail_Number', 'OriginCityName', 'OriginState', 'DestCityName',
-                         'DestState', 'Flight_Number_Reporting_Airline', 'CRSElapsedTime'], axis=1)
+                         'DestState', 'Flight_Number_Reporting_Airline', 'CRSElapsedTime', 'FlightDate'], axis=1)
 
 
 def make_flight_date_canonical(joint_df):
@@ -163,7 +181,6 @@ def add_weather_data(joint_df, path_to_weather):
     return pd.concat([joint_df, weather_data_rows], axis=1)
 
 
-
 def factorize_delay(joint_df):
     # fix DelayFactor to numeric
     delay_factor = joint_df['DelayFactor'].factorize()
@@ -178,7 +195,6 @@ def cross_holidays(joint_df):
     holidays = cal.holidays(start=joint_df['FlightDate'].min(), end=joint_df[
         'FlightDate'].max())
     joint_df["is_holiday"] = joint_df["FlightDate"].isin(holidays)
-    joint_df = joint_df.drop(['FlightDate'], axis=1)
     return joint_df
 
 
@@ -189,5 +205,3 @@ def convert_date(str):
 
 
 
-def drop_outliers(jointDf):
-    pass
